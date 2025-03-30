229b78ca8eef80edd56472f90a7f1edf304c65fc
minseung Kim <msk7262@gmail.com>
2025-03-30 23:58:51 +0900

    커밋

---
+++ section01/chapter07.js
@@ -0,0 +1,8 @@
+// 1. 대입 연산자
+let var1 = 1;
+
+// 2. 산술 연산자
+let num1 = 3 + 2;
+let num2 = 3 - 2;
+let num3 = 3 * 2;
+let num4 = 3 / 2;
---
+++ section01/chapter11.js
@@ -0,0 +1,14 @@
+// 함수선언
+function getArea(width, height) {
+  let area = width * height;
+
+  return area;
+}
+
+let area1 = getArea(10, 20);
+console.log(area1);
+
+let area2 = getArea(30, 20);
+console.log(area2);
+
+getArea(10, 20);
