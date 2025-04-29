44754b0d4d081413dbd47250e19d056896c457a5
minseung Kim <msk7262@gmail.com>
2025-04-29 19:46:09 +0900

    add

--- section01/chapter11.js
+++ section01/chapter11.js
@@ -1,9 +1,4 @@
-// 함수선언
-function getArea(width, height) {
-  let area = width * height;
-
-  return area;
-}
+// 함수
 
 let area1 = getArea(10, 20);
 console.log(area1);
@@ -12,3 +7,17 @@ let area2 = getArea(30, 20);
 console.log(area2);
 
 getArea(10, 20);
+
+// 호스팅
+// --> 끌어올리다 라는 뜻
+function getArea(width, height) {
+  function another() {
+    // 중첩 함수
+    console.log("another");
+  }
+
+  another();
+  let area = width * height;
+
+  return area;
+}
